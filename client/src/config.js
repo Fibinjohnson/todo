9dee77a46fac59dded6d57ee9924c3ff1334561d
Fibin Johnson <128117075+Fibinjohnson@users.noreply.github.com>
2023-08-30 23:11:21 +0530

    dev

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // Other configuration options...
-    apiUrl: 'http://localhost:3001/api', // Update this URL
+    apiUrl: 'http://16.171.149.49/api', // Update this URL
 };
 
 export default config;
